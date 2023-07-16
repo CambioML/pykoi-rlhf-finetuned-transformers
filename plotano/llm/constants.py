80af8340a7a42437635e930c8aadc95a5bab9f15
Jared Wilber <jdwlbr@gmail.com>
2023-07-15 17:19:53 -0700

    revert

--- plotano/llm/constants.py
+++
@@ -1,9 +0,0 @@
-"""Constants for the LLM"""
-from enum import Enum
-
-
-class LlmName(Enum):
-    """Enumeration of the available LLMs."""
-    LLAMA_CPP = "llama_cpp"
-    OPENAI = "openai"
-    SAGEMAKER = "sagemaker"
