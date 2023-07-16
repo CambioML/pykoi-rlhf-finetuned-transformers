2a7c38e86d4d2bb4e522ebb5b46534e3fcf60fb3
CambioML <wanwanaiai45@gmail.com>
2023-07-16 10:21:27 -0700

    add cambio root import and update openai_chatbot example to use cambio/cb to import everything

--- example/openai_chatbot.py
+++ example/openai_chatbot.py
@@ -1,21 +1,19 @@
-from plotano.application import Application
-from plotano.component.base import Chatbot
-from plotano.llm.model_factory import ModelFactory
+import plotano.cambio as cb
 
 
 # enter openai api key here
 api_key = ""
 
 # Creating an OpenAI model
-openai_model = ModelFactory.create_model(
+openai_model = cb.ModelFactory.create_model(
     "openai",
     api_key=api_key)
 
 # Creating a chatbot with the OpenAI model
-chatbot = Chatbot(openai_model)
+chatbot = cb.Chatbot(openai_model)
 
 # Starting the application (assuming the app is an instance of the Application class)
 # Create the application
-app = Application()
+app = cb.Application()
 app.add_component(chatbot)
 app.run()
---
+++ plotano/cambio.py
@@ -0,0 +1,3 @@
+from plotano.application import Application
+from plotano.component.base import Chatbot, Dropdown
+from plotano.llm.model_factory import ModelFactory
