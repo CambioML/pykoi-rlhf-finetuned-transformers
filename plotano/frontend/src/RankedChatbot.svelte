0486ab6a5e1ccc6390d9a784e553a42911d8d3c4
Jared Wilber <jdwlbr@gmail.com>
2023-07-20 16:53:55 -0700

    clean up css

--- plotano/frontend/src/RankedChatbot.svelte
+++ plotano/frontend/src/RankedChatbot.svelte
@@ -29,6 +29,46 @@
     $chatLog = [...formattedRows];
   }
 
+  const askModel = async (event) => {
+    event.preventDefault(); // Prevents page refresh
+    mymessage = messageplaceholder;
+    messageplaceholder = "";
+    chatLoading = true;
+    let currentEntry = {
+      id: $chatLog.length + 1,
+      question: mymessage,
+      answer: "Loading...",
+      vote_status: "na",
+    };
+    console.log("adding to log here");
+    $chatLog = [...$chatLog, currentEntry];
+
+    const response = await fetch(`/chat/${mymessage}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: mymessage,
+      }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      currentEntry["answer"] = data["answer"];
+      // $chatLog[$chatLog.length - 1] = currentEntry;
+      chatLog.update((state) => {
+        state[state.length - 1] = currentEntry;
+        return state;
+      });
+      //   insertToDatabase(currentEntry);
+    } else {
+      const err = await response.text();
+      alert(err);
+    }
+    chatLoading = false;
+  };
+
   function scrollToView(node) {
     setTimeout(() => {
       node.scrollIntoView({ behavior: "smooth" });
@@ -44,6 +84,31 @@
   $: dots = ".".repeat(dotState).padEnd(3);
 
   $: console.log("chat updated!", $chatLog);
+
+  async function logVote(vote, index) {
+    const messageLog = $chatLog[index];
+    messageLog.vote = vote;
+    const feedbackUpdate = {
+      id: index + 1, // increment bc sqlite 1-indexed
+      vote_status: vote,
+    };
+    console.log(feedbackUpdate);
+    console.log(feedbackUpdate);
+    const response = await fetch("http://127.0.0.1:5000/chat/qa_table/update", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(feedbackUpdate),
+    });
+
+    if (response.ok) {
+      console.log("update worked!", response);
+    } else {
+      const err = await response.text();
+      alert(err);
+    }
+  }
 
   function handleClick() {
     select(this.parentNode)
@@ -67,55 +132,144 @@
     </p>
     <button>Download Data</button>
   </div>
-  <section class="chatbox">
-    <div class="chat-log">
-      {#each $chatLog as message, index (index)}
-        <div
-          class="chat-message"
-          use:scrollToView={index === $chatLog.length - 1}
-        >
-          <div class="chat-message-center">
-            <div class="avatar">
-              <!-- <img src={logo} alt="SvelteKit" /> -->
-            </div>
-            <div class="message-content">
-              <div class="question">
-                <h5 class="bold">Question:</h5>
-                <p>{message.question}</p>
+  <div class="ranked-chat">
+    <section class="chatbox">
+      <div class="chat-log">
+        {#each $chatLog as message, index (index)}
+          <div
+            class="chat-message"
+            use:scrollToView={index === $chatLog.length - 1}
+          >
+            <div class="chat-message-center">
+              <div class="avatar">
+                <!-- <img src={logo} alt="SvelteKit" /> -->
               </div>
-              <div class="answers">
-                <div class="answer" on:click={handleClick}>
-                  <h5 class="bold underline">Response 1:</h5>
-                  <p>{message.answer}</p>
+              <div class="message-content">
+                <div class="question">
+                  <h5 class="bold">Question:</h5>
+                  <p>{message.question}</p>
                 </div>
-                <div class="answer" on:click={handleClick}>
-                  <h5 class="bold underline">Response 2:</h5>
-                  <p>{message.answer}</p>
+                <div class="answers">
+                  <div
+                    class="answer"
+                    on:click={handleClick}
+                    on:keydown={handleClick}
+                  >
+                    <h5 class="bold underline">Response 1:</h5>
+                    <p>{message.answer}</p>
+                  </div>
+                  <div
+                    class="answer"
+                    on:click={handleClick}
+                    on:keydown={handleClick}
+                  >
+                    <h5 class="bold underline">Response 2:</h5>
+                    <p>{message.answer}</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      {/each}
+        {/each}
+      </div>
+    </section>
+    <div class="chat-input-holder">
+      <form on:submit={askModel} class="chat-input-form">
+        <input
+          bind:value={messageplaceholder}
+          class="chat-input-textarea"
+          placeholder="Type Message Here"
+        />
+        <button
+          class="btnyousend {messageplaceholder === '' ? '' : 'active'}"
+          type="submit">{chatLoading ? dots : "Send"}</button
+        >
+      </form>
+      <p class="message">Note - may produce inaccurate information.</p>
     </div>
-  </section>
+  </div>
 </div>
 
 <style>
+  .ranked-chat {
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 80% 20%;
+  }
+
+  .message {
+    font-size: var(--smallText);
+    padding-left: 40px;
+    padding-right: 40px;
+    margin: 0 auto;
+  }
+
+  .chat-input-holder {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 24px;
+    width: 100%;
+    max-width: 640px;
+    margin: auto;
+  }
+
+  .chat-input-textarea {
+    background-color: var(--lightgrey);
+    cursor: pointer;
+    width: 100%;
+    border-radius: 5px;
+    border: var(--line);
+    border-color: none;
+    margin: 12px;
+    outline: none;
+    padding: 12px;
+    color: var(--black);
+    font-size: var(--baseFontSize);
+    box-shadow: var(--shadow-md);
+    flex: 3;
+    border-radius: 0px;
+    border-right: 0px;
+  }
+
+  .chat-input-form {
+    display: flex;
+    width: 100%;
+  }
+
+  .btnyousend {
+    border-radius: 0px;
+    margin-top: 12px;
+    margin-bottom: 12px;
+    margin-left: -15px;
+    background: var(--primary);
+    color: var(--black);
+    opacity: 0.5;
+    transition: all 0.3s;
+  }
+
+  .active {
+    opacity: 1;
+  }
+
   .green {
     border-bottom: 2px solid var(--green);
   }
+
   .red {
     border-bottom: 2px solid var(--red);
   }
+
   .instructions {
-    /* border: 2px solid black; */
     text-align: center;
     padding: 5%;
   }
+
   .instructions h5 {
     text-align: left;
   }
+
   .instructions p {
     font-size: var(--smallText);
     text-align: left;
@@ -124,19 +278,23 @@
   .instructions button {
     font-size: var(--smallText);
   }
+
   .ranked-feedback-container {
     display: grid;
     grid-template-columns: 20% 80%;
   }
+
   .underline {
     border-bottom: var(--line);
   }
+
   .bold {
     font-weight: bold;
     font-size: var(--smallText);
     margin: 0;
     padding: 0;
   }
+
   .chatbox {
     display: flex;
     flex-direction: column;
@@ -146,6 +304,7 @@
     box-sizing: border-box;
     width: 95%;
     margin: auto;
+    height: 100%;
   }
 
   .chat-log {
@@ -192,9 +351,6 @@
   }
 
   .message-content .answers {
-    /* display: flex;
-    flex-direction: column;
-    align-items: flex-end; */
     display: grid;
     grid-template-columns: 49% 49%;
     gap: 2%;
