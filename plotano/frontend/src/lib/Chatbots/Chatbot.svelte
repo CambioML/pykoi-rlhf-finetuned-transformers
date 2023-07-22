5983242698cd9b362181f567262b4bfbdf055a1a
Jared Wilber <jdwlbr@gmail.com>
2023-07-22 16:25:53 -0700

    redesign chatbot

--- plotano/frontend/src/lib/Chatbots/Chatbot.svelte
+++ plotano/frontend/src/lib/Chatbots/Chatbot.svelte
@@ -132,10 +132,78 @@
       alert(err);
     }
   }
-
-  $: console.log("chat updated!", $chatLog);
 </script>
 
+<div class="ranked-feedback-container">
+  <div class="instructions">
+    <h5 class="underline bold">Vote Feedback Instructions</h5>
+    <p>
+      Ask a question to receive an answer from the chatbot. If the response is
+      satisfactory, click on the <span class="inline-button green">👍</span>
+      button. If the repsonse is not satisfactory, click on the
+      <span class="inline-button red">👎</span> button.
+    </p>
+    <button>Download Data</button>
+  </div>
+  <div class="ranked-chat">
+    <section class="chatbox">
+      <div class="chat-log">
+        {#each $chatLog as message, index (index)}
+          <div
+            class="chat-message"
+            use:scrollToView={index === $chatLog.length - 1}
+          >
+            <div class="chat-message-center">
+              <div class="avatar">
+                <!-- <img src={logo} alt="SvelteKit" /> -->
+              </div>
+              <div class="message-content">
+                <div class="question">
+                  <h5 class="bold">Question:</h5>
+                  <p>{message.question}</p>
+                </div>
+                <div class="answers">
+                  <div class="answer">
+                    <h5 class="bold">Response:</h5>
+                    <p>{message.answer}</p>
+                    {#if feedback}
+                      <div class="feedback-buttons">
+                        <button
+                          on:click={() => logVote("up", index)}
+                          class="small-button thumbs-up">👍</button
+                        >
+                        <button
+                          on:click={() => logVote("down", index)}
+                          class="small-button thumbs-down">👎</button
+                        >
+                      </div>
+                    {/if}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        {/each}
+      </div>
+    </section>
+    <div class="chat-input-holder">
+      <form on:submit={askModel} class="chat-input-form">
+        <input
+          bind:value={messageplaceholder}
+          class="chat-input-textarea"
+          placeholder="Type Question Here"
+        />
+        <button
+          class="btnyousend {messageplaceholder === '' ? '' : 'active'}"
+          type="submit">{chatLoading ? dots : "Send"}</button
+        >
+      </form>
+      <p class="message">Note - may produce inaccurate information.</p>
+    </div>
+  </div>
+</div>
+
+<!-- 
 <section class="chatbox">
   <div class="chat-log">
     {#each $chatLog as message, index (index)}
@@ -144,22 +212,27 @@
         use:scrollToView={index === $chatLog.length - 1}
       >
         <div class="chat-message-center">
-          <div class="avatar">
-            <!-- <img src={logo} alt="SvelteKit" /> -->
-          </div>
           <div class="message-content">
-            <p>Q: {message.question}</p>
-            <p>A: {message.answer}</p>
-            {#if feedback}
-              <button
-                on:click={() => logVote("up", index)}
-                class="small-button thumbs-up">👍</button
-              >
-              <button
-                on:click={() => logVote("down", index)}
-                class="small-button thumbs-down">👎</button
-              >
-            {/if}
+            <div class="question">
+              <h5 class="bold">Question:</h5>
+              <p>{message.question}</p>
+            </div>
+            <div class="answers">
+              <div class="answer">
+                <h5 class="bold underline">Response:</h5>
+                <p>A: {message.answer}</p>
+                {#if feedback}
+                  <button
+                    on:click={() => logVote("up", index)}
+                    class="small-button thumbs-up">👍</button
+                  >
+                  <button
+                    on:click={() => logVote("down", index)}
+                    class="small-button thumbs-down">👎</button
+                  >
+                {/if}
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -180,9 +253,8 @@
     </form>
     <p class="message">Note - may produce inaccurate information.</p>
   </div>
-</section>
+</section> -->
 
-<!-- STYLING -->
 <style>
   .small-button {
     margin-left: 10px;
@@ -192,7 +264,15 @@
     padding: 6px 10px;
     cursor: pointer;
     box-shadow: none;
-    font-size: calc(var(--baseFontSize) * 1.1);
+    font-size: var(--smallText);
+  }
+
+  .feedback-buttons {
+    /* border: 1px solid black; */
+    text-align: center;
+    margin: auto;
+    width: 15%;
+    /* background: var(--lightGrey); */
   }
 
   .small-button:hover {
@@ -209,27 +289,28 @@
   .thumbs-down::selection {
     background: var(--red);
   }
+  .ranked-chat {
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 100%;
+    grid-template-rows: 80% 20%;
+  }
+
   .message {
     font-size: var(--smallText);
-    padding: 0;
+    padding-left: 40px;
+    padding-right: 40px;
     margin: 0 auto;
-  }
-  .chatbox {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: calc(100vh - var(--headerHeight));
-    background-color: var(--white);
   }
 
   .chat-input-holder {
-    display: flex; /* Add this line */
-    justify-content: space-between; /* Add this line to align items to the left and right of the holder */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     padding: 24px;
     width: 100%;
     max-width: 640px;
     margin: auto;
-    text-align: center;
   }
 
   .chat-input-textarea {
@@ -245,71 +326,14 @@
     color: var(--black);
     font-size: var(--baseFontSize);
     box-shadow: var(--shadow-md);
-  }
-
-  .chat-log {
-    flex: 1;
-    overflow-y: auto;
-    text-align: left;
-  }
-
-  /*  */
-
-  .chat-message {
-    background-color: var(--white);
-    border-bottom: var(--line);
-  }
-  .chat-message.chatgpt {
-    background-color: var(--lightGrey);
-    color: var(--black);
-  }
-
-  .chat-message-center {
-    display: flex;
-    max-width: 640px;
-    margin-left: auto;
-    margin-right: auto;
-    padding: 12px;
-    padding-left: 24px;
-    padding-right: 24px;
-    align-items: center;
-    /* border-top: var(--line); */
-  }
-
-  .avatar,
-  img {
-    background: var(--lightgrey);
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-    margin-right: 5px;
-  }
-
-  .avatar.chatgpt {
-    background: var(--primary);
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-  }
-
-  .message {
-    padding-left: 40px;
-    padding-right: 40px;
-  }
-
-  .chat-input-holder {
-    display: flex;
-    flex-direction: column; /* Stacks children vertically */
-    align-items: center; /* Center the items */
-    padding: 24px;
-    width: 100%;
-    max-width: 640px;
-    margin: auto;
+    flex: 3;
+    border-radius: 0px;
+    border-right: 0px;
   }
 
   .chat-input-form {
-    display: flex; /* Make the input and button side by side */
-    width: 100%; /* Take the full width of the parent */
+    display: flex;
+    width: 100%;
   }
 
   .btnyousend {
@@ -327,28 +351,108 @@
     opacity: 1;
   }
 
-  .chat-input-textarea {
-    /* Input will take the remaining space */
-    flex: 3;
-    border-radius: 0px;
-    border-right: 0px;
+  .green {
+    border-bottom: 2px solid var(--green);
+  }
+
+  .red {
+    border-bottom: 2px solid var(--red);
+  }
+
+  .instructions {
+    text-align: center;
+    padding: 5%;
+  }
+
+  .instructions h5 {
+    text-align: left;
+  }
+
+  .instructions p {
+    font-size: var(--smallText);
+    text-align: left;
+  }
+
+  .instructions button {
+    font-size: var(--smallText);
+  }
+
+  .ranked-feedback-container {
+    display: grid;
+    grid-template-columns: 20% 80%;
+  }
+
+  .underline {
+    border-bottom: var(--line);
+  }
+
+  .bold {
+    font-weight: bold;
+    font-size: var(--smallText);
+    margin: 0;
+    padding: 0;
+  }
+
+  .chatbox {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: calc(100vh - var(--headerHeight));
+    background-color: var(--white);
+    box-sizing: border-box;
+    width: 95%;
+    margin: auto;
+    height: 100%;
+  }
+
+  .chat-log {
+    flex: 1;
+    overflow-y: auto;
+    padding: 0 10px;
+    box-sizing: border-box;
+  }
+
+  .chat-message {
+    background-color: var(--white);
+    border-bottom: var(--line);
+    box-sizing: border-box;
+  }
+
+  .chat-message-center {
+    display: flex;
+    flex-direction: column;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 12px;
+    box-sizing: border-box;
   }
 
   .message-content {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: start;
+    box-sizing: border-box;
   }
 
-  .message-content {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
+  .message-content .question {
+    text-align: left;
+    border: 1px solid var(--grey);
+    padding: 5px;
+    /* margin-bottom: 10px; */
+    background-color: var(--lightGrey);
   }
 
-  .message-content p {
-    margin-right: 10px;
+  .message-content .answer {
+    display: inline-block;
+    text-align: left;
+    padding: 10px;
+    border: 1px solid var(--black);
+  }
+
+  .message-content .answers {
+    display: grid;
+    grid-template-columns: 100%;
+    gap: 0%;
+    width: 100%;
+    margin: auto;
   }
 </style>
