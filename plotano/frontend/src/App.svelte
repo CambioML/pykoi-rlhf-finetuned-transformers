6f460104f151944917c3bb7f091e976ba5bc8ee7
Jared Wilber <jdwlbr@gmail.com>
2023-07-22 10:59:35 -0700

    Clean up App

--- plotano/frontend/src/App.svelte
+++ plotano/frontend/src/App.svelte
@@ -16,10 +16,7 @@
     });
 </script>
 
-<Feedback />
-<Chat />
-
-<!-- {#each $components as component}
+{#each $components as component}
   {#if component.svelte_component === "Feedback"}
     <Feedback {...component.props} />
   {/if}
@@ -30,13 +27,4 @@
   {#if component.svelte_component === "Dropdown"}
     <Dropdown {...component.props} />
   {/if}
-{/each} -->
-
-<style>
-  .footer-logo {
-    font-size: var(--smallText);
-    margin: auto;
-    width: 100%;
-    margin-top: 1rem;
-  }
-</style>
+{/each}
