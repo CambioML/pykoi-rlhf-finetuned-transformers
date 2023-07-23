bc953e8964a3c4bc462e7a024e0f2776a42955ce
Jared Wilber <jdwlbr@gmail.com>
2023-07-22 21:03:45 -0700

    add draft of evaluation page for rlhf

---
+++ plotano/frontend/src/lib/Evaluation/Cell.svelte
@@ -0,0 +1,30 @@
+<!-- Cell.svelte -->
+<script>
+  export let content;
+  export let isActive;
+</script>
+
+<div class="cell" class:active={isActive}>
+  <div class="content">
+    {isActive
+      ? content
+      : content.length > 40
+      ? `${content.substring(0, 40)}...`
+      : content}
+  </div>
+</div>
+
+<style>
+  .cell {
+    max-height: 40px;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+
+  .cell.active {
+    max-height: 100%;
+    overflow: visible;
+    white-space: normal;
+  }
+</style>
