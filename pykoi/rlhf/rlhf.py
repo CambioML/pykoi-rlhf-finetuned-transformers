b9284283b72b7b69d64c2894f4e0bd9020b13b3f
Rachel Hu <goldpiggy@berkeley.edu>
2023-07-26 22:01:41 +0000

    notebooks examples

--- pykoi/rlhf/rlhf.py
+++ pykoi/rlhf/rlhf.py
@@ -426,6 +426,7 @@ class SFT(Trainer):
         )
         self.model = AutoModelForCausalLM.from_pretrained(
             self._rlhf_config.base_model_path,
+            trust_remote_code=True,
             load_in_8bit=self._rlhf_config.load_in_8bit,
             device_map=self._rlhf_config.device_map,
         )
@@ -806,3 +807,160 @@ class RewardTrainer(Trainer):
         """
         self.train()
         self.save(output_path)
+
+
+############################################################
+## Step 3: RL Trainer
+############################################################
+
+
+class RL(Trainer):
+    def __init__(self, rlhf_config: RLHFConfig):
+        self._rlhf_config = rlhf_config
+        self.accelerator = Accelerator()
+        self.num_proc = self._rlhf_config.num_workers if not self._rlhf_config.streaming else None
+        set_seed(rlhf_config.seed) ## TODO: how to set seed properly in __init__?
+
+        self.ppo_config=PPOConfig(
+            steps=self._rlhf_config.total_ppo_epochs,
+            model_name=self._rlhf_config.base_model_path,
+            learning_rate=self._rlhf_config.learning_rate,
+            batch_size=self._rlhf_config.ppo_batch_size,
+            mini_batch_size=self._rlhf_config.mini_batch_size,
+            gradient_accumulation_steps=self._rlhf_config.gradient_accumulation_steps,
+            optimize_cuda_cache=True,
+            early_stopping=self._rlhf_config.early_stopping,
+            target_kl=self._rlhf_config.target_kl,
+            ppo_epochs=self._rlhf_config.ppo_epochs,
+            seed=self._rlhf_config.seed,
+            init_kl_coef=self._rlhf_config.init_kl_coef,
+            adap_kl_ctrl=self._rlhf_config.adap_kl_ctrl,
+            accelerator_kwargs=self._rlhf_config.accelerator_kwargs,
+            )
+        
+        ## Load the base model and tokenizer and define the PPO Trainer for RL
+        self.base_tokenizer = self.create_tokenizer(rlhf_config.base_model_path)
+        self.base_dataset=self.create_dataset(self.base_tokenizer)
+        self.base_model = AutoModelForCausalLMWithValueHead.from_pretrained(
+            rlhf_config.base_model_path,
+            load_in_8bit=rlhf_config.load_in_8bit,
+            # is_loaded_in_8bit = True, # TODO TypeError: LlamaForCausalLM.__init__() got an unexpected keyword argument 'is_loaded_in_8bit'
+            # torch_dtype=torch.float16, 
+            device_map={"": Accelerator().local_process_index},
+            peft_config=rlhf_config.lora_config_rl, 
+        )
+        self.ppo_trainer = PPOTrainer(
+            config=self.ppo_config,
+            model=self.base_model,
+            ref_model=None,
+            tokenizer=self.base_tokenizer,
+            dataset=self.base_dataset,
+            data_collator=self.data_collator,
+            # optimizer=optimizer,
+            # peft_config=lora_config, ## PPOTrainer doesn't support parameter peft_config
+        )
+        self.base_kwargs = {
+            # "min_length": -1,
+            "top_k": rlhf_config.top_k,
+            "top_p": rlhf_config.top_p,
+            "do_sample": rlhf_config.do_sample,
+            "pad_token_id": self.base_tokenizer.pad_token_id,
+            "eos_token_id": rlhf_config.eos_token_id,
+            "max_length": rlhf_config.output_max_length
+        }
+
+        ## Load the reward model and tokenizer and define the reward pipeline
+        self.reward_tokenizer = self.create_tokenizer(rlhf_config.reward_model_path)
+        self.reward_dataset=self.create_dataset(self.reward_tokenizer)
+        self.reward_model = AutoModelForSequenceClassification.from_pretrained(
+            rlhf_config.reward_model_path, 
+            num_labels=1,
+            # torch_dtype=torch.bfloat16,
+            load_in_8bit=True,
+            device_map={"": Accelerator().local_process_index}
+        )
+        self.reward_kwargs = {
+            "return_all_scores": True,
+            "function_to_apply": "none",
+            "batch_size": self._rlhf_config.ppo_batch_size,
+            "truncation": True,
+            "max_length": self._rlhf_config.output_max_length
+        }
+        self.reward_pipe = pipeline(
+            "sentiment-analysis",
+            model=self.reward_model,
+            # device_map={"": Accelerator().local_process_index},
+            # model_kwargs={"load_in_8bit": True},
+            model_kwargs=self.reward_kwargs,
+            tokenizer=self.reward_tokenizer,
+            return_token_type_ids=False,
+        )
+
+        
+    def create_tokenizer(self, model_name):
+        tokenizer = AutoTokenizer.from_pretrained(model_name)
+        if getattr(tokenizer, "pad_token", None) is None:
+            tokenizer.pad_token = tokenizer.eos_token
+        return tokenizer
+
+
+    def data_collator(self, data):
+        return dict((key, [d[key] for d in data]) for key in data[0])
+
+
+    def create_dataset(self, tokenizer):
+        """
+        Build dataset for training. This builds the dataset from `load_dataset`, one should
+        customize this function to train the model on its own dataset.
+        """
+        ds = load_dataset(self._rlhf_config.dataset_name, 
+                          data_dir=self._rlhf_config.dataset_subset_rl, 
+                          split=self._rlhf_config.split)
+        ds = ds.select(range(self._rlhf_config.dataset_subset_rl_train))
+
+        def preprocess_function(examples):
+            queries = ["Question: " + q + "\n\nAnswer: " for q in examples["question"]]
+            input_ids = [tokenizer(q, truncation=True)["input_ids"] for q in queries]
+            return {"query": queries, "input_ids": input_ids}
+
+        ds = ds.map(
+            preprocess_function,
+            batched=True,
+            num_proc=self.num_proc,
+            remove_columns=ds.column_names,
+        )
+        ds = ds.filter(lambda x: len(x["input_ids"]) < self._rlhf_config.max_seq_length, 
+                       batched=False)
+        ds.set_format(type="torch") ## TODO
+
+        return ds
+
+
+    def train(self):
+        for epoch, batch in tqdm(enumerate(self.ppo_trainer.dataloader)):
+            if epoch >= self._rlhf_config.total_ppo_epochs:
+                break
+            ## embed the questions and responses to tensors
+            question_tensors = batch["input_ids"]
+            response_tensors = self.ppo_trainer.generate(
+                question_tensors,
+                return_prompt=False,
+                length_sampler=LengthSampler(self._rlhf_config.output_min_length, 
+                                             self._rlhf_config.output_max_length),
+                **self.base_kwargs,
+            )
+            batch["response"] = self.base_tokenizer.batch_decode(response_tensors, 
+                                                                 skip_special_tokens=True)
+            # compute rewards and run PPO
+            texts = [q + r for q, r in zip(batch["query"], batch["response"])]
+            pipe_outputs = self.reward_pipe(texts, **self.reward_kwargs)
+            rewards = [torch.tensor(output[0]["score"] - self._rlhf_config.reward_baseline) \
+                       for output in pipe_outputs]
+            stats = self.ppo_trainer.step(question_tensors, response_tensors, rewards)
+            self.ppo_trainer.log_stats(stats, batch, rewards)
+
+            ## save weights
+            if self._rlhf_config.save_freq and epoch and \
+                epoch % self._rlhf_config.save_freq == 0:
+                self.ppo_trainer.save_pretrained(
+                    os.path.join(self._rlhf_config.output_dir, f"rlhf_rl_step_{epoch}"))
