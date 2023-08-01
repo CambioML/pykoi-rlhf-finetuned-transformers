71117d1df274f0b672e6dc10406ed76a6d0c187f
Jared Wilber <jdwlbr@gmail.com>
2023-07-31 18:16:59 -0700

    debug state

--- pykoi/frontend/src/App.svelte
+++ pykoi/frontend/src/App.svelte
@@ -58,3 +58,5 @@
     {...$selectedPage.props}
   />
 {/if}
+
+<ComparisonChart />
--- pykoi/frontend/vite.config.js
+++ pykoi/frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [svelte()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        format: "iife",
+        manualChunks: undefined, // Disable code splitting
+      },
+    },
+  },
+};
