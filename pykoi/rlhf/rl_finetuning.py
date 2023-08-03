cfd83094a0b9538c231d463c6863d63a5b152a63
Rachel Hu <goldpiggy@berkeley.edu>
2023-08-03 15:27:02 -0700

    add log to rlhf step 3 to visualize

--- pykoi/rlhf/rl_finetuning.py
+++ pykoi/rlhf/rl_finetuning.py
@@ -8,7 +8,8 @@ from pykoi.db.constants import (
 )
 
 import os
-
+import json
+import numpy as np
 import torch
 from pykoi.db.qa_database import QuestionAnswerDatabase
 from accelerate import Accelerator
@@ -108,6 +109,7 @@ class RLFinetuning(Trainer):
             "pad_token_id": self.base_tokenizer.pad_token_id,
             "eos_token_id": rlhf_config.eos_token_id,
         }
+        self.ppo_log_stats_dict = {} # initialize the log stats dict
 
     def create_tokenizer(self, model_name):
         """
@@ -237,8 +239,12 @@ class RLFinetuning(Trainer):
                 for output in pipe_outputs
             ]
             stats = self.ppo_trainer.step(question_tensors, response_tensors, rewards)
-            self.ppo_trainer.log_stats(stats, batch, rewards)
+
+            ## log stats
+            self.log_stats_to_json(epoch=epoch, stats=stats, reward=rewards[0])
+            # self.ppo_trainer.log_stats(stats, batch, rewards)
             print("stats: {}\n\n\n rewards: {}\n\n\n".format(stats, rewards))
+            
 
             ## save weights
             if (
@@ -252,7 +258,34 @@ class RLFinetuning(Trainer):
                         "checkpoints",
                         f"checkpoints_epoch_{epoch}",
                     )
-                self.ppo_trainer.model.save(save_checkpoints_path)
+                self.ppo_trainer.save_pretrained(save_checkpoints_path)
+
+    def log_stats_to_json(self, epoch, stats, reward, filename="ppo_log_stats.json"):
+        """
+        Logs the PPO stats to a json file.        
+        Args:
+            epoch (int): The current epoch.
+            stats (dict): The PPO stats.
+            reward (float): The reward.
+            filename (str, optional): The name of the json file. Defaults to "ppo_log_stats.json".
+        """
+        logs = self.ppo_log_stats_dict
+        # Add new logs
+        new_log = {}
+        for stat_name, value in stats.items():
+            if isinstance(value, np.ndarray) or isinstance(value, torch.Tensor):
+                new_log[stat_name] = value.tolist()
+            elif isinstance(value, (int, float, str, bool)) or value is None:
+                new_log[stat_name] = value
+            else:
+                print(f"Warning: Skipping non-serializable stat '{stat_name}' of type {type(value).__name__}")
+        new_log['reward'] = reward.tolist()
+        # Write logs to file
+        logs[f"epoch{epoch}"] = new_log
+        with open(filename, "w") as json_file:
+            json.dump(logs, json_file)
+        # Update the class attribute
+        self.ppo_log_stats_dict = logs
 
     def save(self, output_path=None):
         """
@@ -260,7 +293,7 @@ class RLFinetuning(Trainer):
         output directory specified in the RLHFConfig object.
 
         Args:
-            output_path (str, optional): The path to save the model to. Defaults to None.
+            output_path (str, optional): The path to save the model to. Default to None.
         """
         if output_path is None:
             output_path = os.path.join(
@@ -275,7 +308,7 @@ class RLFinetuning(Trainer):
         is saved to the output directory specified in the RLHFConfig object.
 
         Args:
-            output_path (str, optional): The path to save the model to. Defaults to None.
+            output_path (str, optional): The path to save the model to. Default to None.
         """
         self.train(save_checkpoints_path=output_path)
         self.save(output_path)
