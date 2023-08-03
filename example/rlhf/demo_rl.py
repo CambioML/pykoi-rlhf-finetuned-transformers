da4ef59b8cdbc45bd0cf4fa541f363719aead6e1
Rachel Hu <goldpiggy@berkeley.edu>
2023-08-03 11:46:25 -0700

    rlhf data cleaning

--- example/rlhf/demo_rl.py
+++ example/rlhf/demo_rl.py
@@ -5,20 +5,6 @@
 import pykoi
 
 # use huggingface sft and reward model
-# config = pykoi.RLHFConfig(
-#     base_model_path="elinas/llama-7b-hf-transformers-4.29",  # "meta-llama/Llama-2-7b-hf",
-#     dataset_type="huggingface",  ## "local_db",
-#     dataset_name="goldmermaid/stack_exchange_rank_10k_dataset",  ##"lvwerra/stack-exchange-paired", ## "/home/ubuntu/git/pykoi/example/notebook/qd.db",
-#     dataset_subset_rl="data",
-#     reward_model_path="goldmermaid/rlhf_reward_model",
-#     save_freq=1,
-#     ppo_batch_size=32,
-#     ppo_epochs=1,
-#     total_epochs=1,
-#     output_dir="./models/rlhf_step3_rl",
-# )
-
-# bring your own step1 sft and step 2 reward model
 config = pykoi.RLHFConfig(
     base_model_path="elinas/llama-7b-hf-transformers-4.29",  # "meta-llama/Llama-2-7b-hf",
     dataset_type="huggingface",  ## "local_db",
