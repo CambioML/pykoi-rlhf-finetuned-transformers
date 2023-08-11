89aee6aac2027d1867699248c6c199927e9bec7d
Rachel Hu <goldpiggy@berkeley.edu>
2023-08-11 00:56:08 -0700

    bump up project version to 0.0.4

--- pykoi/__init__.py
+++ pykoi/__init__.py
@@ -13,4 +13,4 @@ from pykoi.rlhf.rw_finetuning import RewardFinetuning
 from pykoi.rlhf.rl_finetuning import RLFinetuning
 from pykoi.rlhf.config import RLHFConfig
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
