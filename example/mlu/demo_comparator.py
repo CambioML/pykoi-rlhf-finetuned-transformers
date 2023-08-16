8ca3423443542681d2c5f6763a357a334f8ce3f1
Rachel Hu <goldpiggy@berkeley.edu>
2023-08-15 18:42:20 -0700

    refactor demo comparator file

--- example/mlu/demo_comparator.py
+++ example/mlu/demo_comparator.py
@@ -24,13 +24,6 @@ hf_tokenizer_1 = AutoTokenizer.from_pretrained(
     device_map="auto",
 )
 
-model_1 = pykoi.chat.llm.huggingface.HuggingfaceModel.create(
-    model=hf_model_1,
-    tokenizer=hf_tokenizer_1,
-    name="falcon-rw-1b",
-    max_length=100,
-)
-
 ###################################################################################
 # Creating a Huggingface model tiiuae/falcon-7b (EC2 g4.2xlarge with 100GB space) #
 ###################################################################################
@@ -48,13 +41,6 @@ hf_tokenizer_2 = AutoTokenizer.from_pretrained(
     trust_remote_code=True,
     load_in_8bit=True,
     device_map="auto",
-)
-
-model_2 = pykoi.chat.llm.huggingface.HuggingfaceModel.create(
-    model=hf_model_2,
-    tokenizer=hf_tokenizer_2,
-    name="falcon-7b",
-    max_length=100,
 )
 
 #########################################################################################
@@ -76,21 +62,22 @@ hf_tokenizer_3 = AutoTokenizer.from_pretrained(
     device_map="auto",
 )
 
-model_3 = pykoi.chat.llm.huggingface.HuggingfaceModel.create(
-    model=hf_model_3,
-    tokenizer=hf_tokenizer_3,
-    name="dolly-v2-3b",
-    max_length=100,
-)
-
 #################################
 # Creating a chatbot comparator #
 #################################
 # pass in a list of models to compare
-chatbot_comparator = pykoi.Compare(models=[model_1, model_2])
-# or add models later
-chatbot_comparator.add(model_3)
+model_name = ["falcon-rw-1b", "falcon-7b", "dolly-v2-3b"]
+models = [hf_model_1, hf_model_2, hf_model_3]
+tokenizers = [hf_tokenizer_1, hf_tokenizer_2, hf_tokenizer_3]
 
+models_list = [
+    pykoi.chat.llm.huggingface.HuggingfaceModel.create(
+        model=model, tokenizer=tokenizer, name=name, max_length=100
+    )
+    for model, tokenizer, name in zip(models, tokenizers, model_name)
+]
+
+chatbot_comparator = pykoi.Compare(models=models_list)
 app = pykoi.Application()
 app.add_component(chatbot_comparator)
 app.run()
