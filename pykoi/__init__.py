19af2882e52eaf40ff48c7da1b50ddcea57d0ffd
Rachel Hu <goldpiggy@berkeley.edu>
2023-08-16 17:00:33 -0700

    add back RetrievalQA in __init__.py, but might have issue for running in SageMaker

--- pykoi/__init__.py
+++ pykoi/__init__.py
@@ -7,7 +7,6 @@ from pykoi.chat.llm.abs_llm import AbsLlm
 from pykoi.chat.llm.model_factory import ModelFactory
 from pykoi.component.base import Chatbot, Dashboard, Dropdown
 from pykoi.component.chatbot_comparator import Compare
-# from pykoi.component.retrieval_qa import RetrievalQA
 from pykoi.component.retrieval_qa import RetrievalQA
 from pykoi.component.nvml import Nvml
 from pykoi.retrieval.llm.retrieval_factory import RetrievalFactory
