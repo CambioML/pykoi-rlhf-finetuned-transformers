077f4870a3d61ccc032de60fd5817c41ad254e95
Jared Wilber <jdwlbr@gmail.com>
2023-08-16 21:09:21 -0700

    reset vite config

--- pykoi/frontend/vite.config.js
+++ pykoi/frontend/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
   plugins: [svelte()],
-  build: {
-    rollupOptions: {
-      output: {
-        format: "iife",
-        manualChunks: undefined, // Disable code splitting
-      },
-    },
-  },
-};
+});
