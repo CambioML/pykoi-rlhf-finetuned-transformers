f86bc0148a155d649093727d8a18dacc11a50f7e
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-08-30 13:43:56 -0700

    add dotenv for OpenAI env variable

--- example/flex/flex_demo.py
+++ example/flex/flex_demo.py
@@ -3,11 +3,14 @@
 import os
 import argparse
 import pykoi
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def main(**kargs):
     # enter openai api key here
-    os.environ["OPENAI_API_KEY"] = "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"
+    # os.environ["OPENAI_API_KEY"] = "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"
     os.environ["DOC_PATH"] = os.path.join(os.getcwd(), "temp/docs")
     os.environ["VECTORDB_PATH"] = os.path.join(os.getcwd(), "temp/vectordb")
     MODEL_SOURCE = "openai"
