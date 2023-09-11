75b31ae6c805efe5a0941e7819171be00a6a394f
ALEX CHEN <weichen6@stanford.edu>
2023-09-11 05:07:02 +0000

    wip

---
+++ docker/README.md
@@ -0,0 +1,5 @@
+# Overview
+In this folder, we create the different dockerfiles for using pykoi.
+
+1. `pykoi-cpu`: The base image for the cpu-based usage.
+2. `pykoi-cpu-custom`: When you run this docker image, try to modify the `app.py` and mount it when running the docker container.
---
+++ docker/pykoi-cpu-custom/app.py
@@ -0,0 +1,34 @@
+"""Demo for the chatbot application using OpenAI endpoint."""
+import pykoi
+
+##########################################################
+# Creating an OpenAI model (requires an OpenAI API key) #
+##########################################################
+# enter openai api key here
+api_key = "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"
+
+# Creating an OpenAI model
+model = pykoi.ModelFactory.create_model(
+    model_source="openai",
+    api_key=api_key)
+
+#####################################
+# Creating a chatbot with the model #
+#####################################
+database = pykoi.QuestionAnswerDatabase(debug=True)
+chatbot = pykoi.Chatbot(model=model, feedback="vote")
+# chatbot = pykoi.Chatbot(model=model, feedback="rank")
+dashboard = pykoi.Dashboard(database=database)
+
+###########################################################
+# Starting the application and add chatbot as a component #
+###########################################################
+# Create the application
+# app = pykoi.Application(debug=False, share=True)
+app = pykoi.Application(
+    host='0.0.0.0',
+    debug=False, 
+    share=True)
+app.add_component(chatbot)
+app.add_component(dashboard)
+app.run()
