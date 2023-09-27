41f7265709a37924283ba8e391222ff0dec98aed
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-09-27 14:26:55 -0700

    add states for DownloadModal to show successful with filepath or failed download

--- pykoi/frontend/src/lib/Chatbots/Components/DownloadModal.svelte
+++ pykoi/frontend/src/lib/Chatbots/Components/DownloadModal.svelte
@@ -1,12 +1,18 @@
 <script>
+    import { onDestroy, onMount } from "svelte";
     import Modal from "../../UIComponents/Modal.svelte";
     export let showModal, table;
     let dialog;
     let file_name = "";
+    const DOWNLOAD_STATE = {
+        FILE_INPUT: 0,
+        DOWNLOADED: 1,
+        FAILED_DOWNLOAD: 2,
+    };
+    let downloadState = DOWNLOAD_STATE.FILE_INPUT;
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        dialog.close();
         const request_body = {
             file_name,
         };
@@ -19,26 +25,56 @@
         });
         const data = await response.json();
         console.log("Download Complete: ", data);
+        if (data.status === "200") {
+            console.log("success");
+            downloadState = DOWNLOAD_STATE.DOWNLOADED;
+        } else {
+            console.log("failed");
+            downloadState = DOWNLOAD_STATE.FAILED_DOWNLOAD;
+        }
     };
+
+    function handleClose() {
+        showModal = false;
+        downloadState = DOWNLOAD_STATE.FILE_INPUT;
+        dialog.close();
+    }
 </script>
 
-<Modal bind:showModal bind:dialog>
+<Modal bind:showModal bind:dialog {handleClose}>
     <h4 slot="header">Download Data</h4>
-    <form on:submit={handleSubmit}>
-        <div class="inputs">
-            <label for="file_name">Filename</label>
-            <input
-                bind:value={file_name}
-                type="text"
-                placeholder="Please enter filename"
-                name="file_name"
-                required
-            />
+    {#if downloadState === DOWNLOAD_STATE.FILE_INPUT}
+        <form on:submit={handleSubmit}>
+            <div class="inputs">
+                <label for="file_name">Filename</label>
+                <input
+                    bind:value={file_name}
+                    type="text"
+                    placeholder="Please enter filename"
+                    name="file_name"
+                    required
+                />
+            </div>
+            <div class="btn-container">
+                <button type="submit">Download</button>
+            </div>
+        </form>
+    {/if}
+    {#if downloadState === DOWNLOAD_STATE.DOWNLOADED}
+        <div>
+            ✅ Data downloaded to ~/pykoi/{file_name}.csv
         </div>
         <div class="btn-container">
-            <button type="submit">Download</button>
+            <button on:click={handleClose}>Close</button>
         </div>
-    </form>
+    {/if}
+    {#if downloadState === DOWNLOAD_STATE.FAILED_DOWNLOAD}
+        <div>⚠️ Download failed. Please try again.</div>
+        <div class="btn-container">
+            <button on:click={() => downloadState = DOWNLOAD_STATE.FILE_INPUT}>Retry</button>
+            <button on:click={handleClose}>Close</button>
+        </div>
+    {/if}
 </Modal>
 
 <style>
