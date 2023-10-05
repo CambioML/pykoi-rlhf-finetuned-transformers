a07b9adede7417357be18c9e60f84221831fc485
Ubuntu <ubuntu@ip-172-31-6-245.us-west-2.compute.internal>
2023-10-05 01:43:07 +0000

    Added mlflow

---
+++ example/rlhf/mlflow/demo_rl.py
@@ -0,0 +1,32 @@
+"""
+accelerate config
+
+LOCAL_DIR=/home/ubuntu/pykoi/pykoi # change this to your local path
+
+export PYTHONPATH=$PYTHONPATH:${LOCAL_DIR}
+
+accelerate launch --num_machines 1  --num_processes 1 --mixed_precision fp16 ${LOCAL_DIR}/example/rlhf/demo_rl.py
+"""
+# accelerate launch --num_machines 1  --num_processes 1 --mixed_precision fp16 example/rlhf/demo_rl.py
+
+from pykoi.rlhf import RLHFConfig
+from pykoi.rlhf import RLFinetuning
+
+
+# use huggingface sft and reward model
+config = RLHFConfig(
+    base_model_path="models/rlhf_step1_sft",    #"elinas/llama-7b-hf-transformers-4.29", 
+    dataset_type="local_db",
+    # dataset_type="huggingface", 
+    # dataset_name="goldmermaid/stack_exchange_rank_10k_dataset",
+    dataset_subset_rl="data",
+    reward_model_path="models/rlhf_step2_rw/", #"cambioml/rlhf_reward_model",
+    save_freq=1,
+    ppo_batch_size=32,
+    ppo_epochs=4,
+    total_epochs=5,
+    output_dir="./models/rlhf_step3_rl",
+)
+
+rlhf_step3_rl = RLFinetuning(config)
+rlhf_step3_rl.train_and_save("./models/rlhf_step3_rl")
---
+++ example/rlhf/mlflow/demo_rw_finetuning.py
@@ -0,0 +1,32 @@
+"""Demo for the reward fine tuning.
+
+python -m example.rlhf.demo_rw_finetuning
+"""
+
+from pykoi.rlhf import RLHFConfig
+from pykoi.rlhf import RewardFinetuning
+from pykoi.chat import RankingDatabase
+from pykoi.chat.db.constants import (
+    RANKING_CSV_HEADER_ID,
+    RANKING_CSV_HEADER_QUESTION,
+    RANKING_CSV_HEADER_UP_RANKING_ANSWER,
+    RANKING_CSV_HEADER_LOW_RANKING_ANSWER)
+
+# get data from local database
+ranking_database = RankingDatabase()
+my_data_pd = ranking_database.retrieve_all_question_answers_as_pandas()
+my_data_pd = my_data_pd[[
+    RANKING_CSV_HEADER_ID,
+    RANKING_CSV_HEADER_QUESTION,
+    RANKING_CSV_HEADER_UP_RANKING_ANSWER,
+    RANKING_CSV_HEADER_LOW_RANKING_ANSWER]]
+
+# analyze the data
+print(my_data_pd)
+print("My local database has {} samples in total".format(my_data_pd.shape[0]))
+
+# run reward model finetuning
+# config = RLHFConfig(dataset_type="local_db")
+config = RLHFConfig(reward_model_path="databricks/dolly-v2-3b")
+rlhf_step2_rft = RewardFinetuning(config)
+rlhf_step2_rft.train_and_save("./models/rlhf_step2_rw")
