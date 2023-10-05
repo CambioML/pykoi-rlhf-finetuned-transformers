b188ea1d09ad42998a363ddfcb6ded4b4f3cfbf8
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-10-05 15:06:22 -0700

    add OVERWRITE download state for checking if file overwrite ok

--- pykoi/frontend/src/lib/Chatbots/Components/DownloadModal.svelte
+++ pykoi/frontend/src/lib/Chatbots/Components/DownloadModal.svelte
@@ -8,11 +8,11 @@
         FILE_INPUT: 0,
         DOWNLOADED: 1,
         FAILED_DOWNLOAD: 2,
+        OVERWRITE: 3,
     };
     let downloadState = DOWNLOAD_STATE.FILE_INPUT;
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const saveToCSV = async (file_name) => {
         const request_body = {
             file_name,
         };
@@ -31,6 +31,19 @@
         } else {
             console.log("failed");
             downloadState = DOWNLOAD_STATE.FAILED_DOWNLOAD;
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch(
+            `/file_exists/?file_name=${file_name}.csv`
+        );
+        const file_exists_response = await response.json();
+        if (file_exists_response.file_exists === true) {
+            downloadState = DOWNLOAD_STATE.OVERWRITE;
+        } else {
+            saveToCSV(file_name);
         }
     };
 
@@ -62,7 +75,7 @@
     {/if}
     {#if downloadState === DOWNLOAD_STATE.DOWNLOADED}
         <div>
-            ✅ Data downloaded to ~/pykoi/{file_name}.csv
+            ✅ Data downloaded to /pykoi/{file_name}.csv
         </div>
         <div class="btn-container">
             <button on:click={handleClose}>Close</button>
@@ -71,8 +84,17 @@
     {#if downloadState === DOWNLOAD_STATE.FAILED_DOWNLOAD}
         <div>⚠️ Download failed. Please try again.</div>
         <div class="btn-container">
-            <button on:click={() => downloadState = DOWNLOAD_STATE.FILE_INPUT}>Retry</button>
+            <button on:click={() => (downloadState = DOWNLOAD_STATE.FILE_INPUT)}>Retry</button>
             <button on:click={handleClose}>Close</button>
+        </div>
+    {/if}
+    {#if downloadState === DOWNLOAD_STATE.OVERWRITE}
+        <div>
+            ⚠️ {file_name}.csv already exists. Do you wish to overwrite it?
+        </div>
+        <div class="btn-container">
+            <button on:click={() => (downloadState = DOWNLOAD_STATE.FILE_INPUT)}>Back</button>
+            <button on:click={() => saveToCSV(file_name)}>Overwrite</button>
         </div>
     {/if}
 </Modal>
