9b7b980752186c075ed00c04554ba55a7b72982e
Larry Yin <larryyin@air23.local>
2023-10-06 02:00:11 -0700

    Added mlflow to rw and rl training examples.

--- example/rlhf/mlflow/demo_rw_finetuning_with_mlflow.py
+++ example/rlhf/mlflow/demo_rw_finetuning_with_mlflow.py
@@ -6,6 +6,9 @@ python -m example.rlhf.demo_rw_finetuning
 from pykoi.rlhf import RLHFConfig
 from pykoi.rlhf import RewardFinetuning
 from pykoi.chat import RankingDatabase
+import mlflow
+import datetime
+
 from pykoi.chat.db.constants import (
     RANKING_CSV_HEADER_ID,
     RANKING_CSV_HEADER_QUESTION,
@@ -25,8 +28,26 @@ my_data_pd = my_data_pd[[
 print(my_data_pd)
 print("My local database has {} samples in total".format(my_data_pd.shape[0]))
 
+# Set up mlflow experiment name.
+# mlflow.set_tracking_uri("http://x.x.x.x:5000")
+experiment = "rlhf_step2_reward"
+current_time = str(datetime.datetime.now())
+mlflow_experiment_name = '/'.join([experiment, current_time])
+mlflow.set_experiment(mlflow_experiment_name)
+
+# Set pykoi parameters.
+reward_model_path = "databricks/dolly-v2-3b"
+trained_model_path = "./models/rlhf_step2_rw"
+
+# Manually log pykoi parameters into mlflow. Torch level parameters are automatically logged.
+mlflow.log_param("pykoi_reward_model_path", reward_model_path)
+mlflow.log_param("pykoi_trained_model_path", trained_model_path)
+
 # run reward model finetuning
 # config = RLHFConfig(dataset_type="local_db")
-config = RLHFConfig(reward_model_path="databricks/dolly-v2-3b")
+config = RLHFConfig(reward_model_path=reward_model_path)
 rlhf_step2_rft = RewardFinetuning(config)
-rlhf_step2_rft.train_and_save("./models/rlhf_step2_rw")
+rlhf_step2_rft.train_and_save(trained_model_path)
+
+# Save the trained reward model into mlflow artifacts.
+mlflow.log_artifacts(trained_model_path)
