cae1fb1cacfe6bc6b18d25794cb50b91ceb5a6ef
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-10-15 22:04:05 -0700

    WIP updating hugginface model to work with new RAG features

--- pykoi/application.py
+++ pykoi/application.py
@@ -606,7 +606,11 @@ class Application:
                 component["component"].retrieval_model.re_init(request_body.file_names)
                 output = component["component"].retrieval_model.run_with_return_source_documents({"query": request_body.prompt})
                 print('output', output, output["result"])
-                if output["source_documents"] == []:
+                if "source_documents" not in output:
+                    print('no source documents', output)
+                    source = ["N/A"]
+                    source_content = ["N/A"]
+                elif output["source_documents"] == []:
                     source = ["N/A"]
                     source_content = ["N/A"]
                 else:
