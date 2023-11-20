2a2f5ec8ef876a52fa04355e34f11c971d79ec20
Jojo Ortiz <ortiz.jroman@gmail.com>
2023-11-19 23:43:06 -0800

    add load_in_8bit to True for model_kwargs

--- pykoi/retrieval/llm/huggingface.py
+++ pykoi/retrieval/llm/huggingface.py
@@ -29,7 +29,8 @@ class HuggingFaceModel(AbsLlm):
                 model_kwargs={
                     "temperature": 0,
                     "max_length": kwargs.get("max_length", 500),
-                    "trust_remote_code": kwargs.get("trust_remote_code", True),
+                    "load_in_8bit": True,
+                    "trust_remote_code": kwargs.get("trust_remote_code", True)
                 },
             )
 
